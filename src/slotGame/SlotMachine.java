1f3c012019c7b626f14f2384960f022d16effd13
kamkom84 <kbd.dimitrov@gmail.com>
2025-02-12 21:20:03 +0200

    cmt:

--- src/slotGame/SlotMachine.java
+++ src/slotGame/SlotMachine.java
@@ -853,7 +853,7 @@ public class SlotMachine extends JFrame {
     private void initializeJackpots() {
         jackpots = new ArrayList<>();
         jackpots.add(new JackpotServer("Minor", 13.53, 0.16, 19.50, 19.97));
-        jackpots.add(new JackpotServer("Major", 21.30, 0.11, 49.50, 40.00));
+        jackpots.add(new JackpotServer("Major", 21.30, 0.11, 49.50, 49.94));
         jackpots.add(new JackpotServer("Mega", 31.13, 0.06, 59.50, 59.92));
 
         bronzeJackpot = jackpots.get(0);
